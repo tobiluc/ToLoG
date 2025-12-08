594ce918a18de123fb569955fcf59d94396e1cdd
Tobias Kohler <tobias.kohler@unibe.ch>
2025-12-08 14:52:14 +0100

    enumerate utility

--- tests/enumerate_tests.hpp
+++ tests/enumerate_tests.hpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <ToLoG/enumerate.hpp>
+#include <ToLoG/utils/enumerate.hpp>
 
 using namespace ToLoG;
 
--- tests/keyvaluevector_tests.cpp
+++ tests/keyvaluevector_tests.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include <ToLoG/KeyValueVector.hpp>
+#include <ToLoG/utils/KeyValueVector.hpp>
 
 using namespace ToLoG;
 
