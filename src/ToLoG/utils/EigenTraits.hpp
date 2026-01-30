2f2f3b6e814ad72bc9b0b7891b134902b14237f9
Tobias Kohler <tobias.kohler@unibe.ch>
2026-01-30 17:55:43 +0100

    EigenTraits Utility

---
+++ src/ToLoG/utils/EigenTraits.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <ToLoG/Traits_fwd.hpp>
+#include <Eigen/Core>
+
+namespace ToLoG
+{
+
+namespace ToLoG
+{
+template<typename FT, int DIM>
+struct is_vector_type<Eigen::Vector<FT,DIM>> : std::true_type {};
+
+template<typename FT, int DIM>
+struct Traits<Eigen::Vector<FT,DIM>>
+{
+    using value_type = FT;
+    using vector_type = Eigen::Vector<FT,DIM>;
+    constexpr static int dim = DIM;
+};
+}
+
+}
