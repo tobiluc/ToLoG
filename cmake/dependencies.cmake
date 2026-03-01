2d330115ef7938d5005f674a681ba7a9cc46276f
Tobias Kohler <tobias.kohler@unibe.ch>
2026-03-01 15:21:44 +0100

    minor

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,5 +2,5 @@ include(FetchContent)
 set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
 set(EXTERNAL_DIR "${PROJECT_SOURCE_DIR}/external")
 
-include(deps/rapidobj)
-include(deps/openmp)
+include("${PROJECT_SOURCE_DIR}/cmake/deps/rapidobj.cmake")
+include("${PROJECT_SOURCE_DIR}/cmake/deps/openmp.cmake")
