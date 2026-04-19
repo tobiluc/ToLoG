9634f12cd2931585d45a539c7d2c7a8037f4b23a
Tobias Kohler <tobias.kohler@unibe.ch>
2026-04-19 11:45:01 +0200

    minor

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -1,6 +1,6 @@
 include(FetchContent)
 set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
-set(EXTERNAL_DIR "${PROJECT_SOURCE_DIR}/external")
+set(TOLOG_EXTERNAL_DIR "${PROJECT_SOURCE_DIR}/external")
 
 include("${PROJECT_SOURCE_DIR}/cmake/deps/rapidobj.cmake")
 include("${PROJECT_SOURCE_DIR}/cmake/deps/openmp.cmake")
--- cmake/deps/rapidobj.cmake
+++ cmake/deps/rapidobj.cmake
@@ -2,7 +2,7 @@ if (NOT TARGET rapidobj::rapidobj)
     FetchContent_Declare(rapidobj
         GIT_REPOSITORY  https://github.com/cgg-bern/rapidobj-double-precision.git
         GIT_TAG         double-precision
-        SOURCE_DIR "${EXTERNAL_DIR}/rapidobj"
+        SOURCE_DIR "${TOLOG_EXTERNAL_DIR}/rapidobj"
     )
     FetchContent_MakeAvailable(rapidobj)
 endif()
