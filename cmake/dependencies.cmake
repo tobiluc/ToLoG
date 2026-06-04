9cd3fc3a7109053612a3f17fda76ea9af78f9df2
Tobias Kohler <tobias.kohler@unibe.ch>
2026-06-04 19:55:10 +0200

    cmaek

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -2,5 +2,5 @@ include(FetchContent)
 set(FETCHCONTENT_UPDATES_DISCONNECTED TRUE)
 set(TOLOG_EXTERNAL_DIR "${PROJECT_SOURCE_DIR}/external")
 
-include("${PROJECT_SOURCE_DIR}/cmake/deps/rapidobj.cmake")
+# include("${PROJECT_SOURCE_DIR}/cmake/deps/rapidobj.cmake")
 include("${PROJECT_SOURCE_DIR}/cmake/deps/openmp.cmake")
